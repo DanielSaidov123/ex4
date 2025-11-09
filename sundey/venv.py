0584bc88c300e2daedb602b1222a1aa94bf21dc4
DanielSaidov123 <ddny3622@gmail.com>
2025-11-09 16:12:01 +0200

    end ex4

--- sundey/venv.py
+++ sundey/venv.py
@@ -4,25 +4,6 @@
 # with open('my_file.txt','r')as f:
 #     temp=f.read()
 
-
-
-# def xor(atbash:dict[str:str],temp:str):
-#     revers={v:k for k , v in atbash.items()  }
-#     d=""
-#     for i in temp:
-#         if i in atbash:
-#             d+=atbash[i]
-#         elif i in revers:
-#             d+=revers[i]
-#         else:
-#             d+=" "
-#     return d
-
-# atbash = {"a":"z","b":"y","c":"x","d":"w","e":"v","f":"u","g":"t","h":"s","i":"r","j":"q","k":"p","l":"o","m":"n"}
-# print(xor(atbash,temp))
-
-xorr = {"a":"z","b":"y","c":"x","d":"w","e":"v","f":"u","g":"t","h":"s","i":"r","j":"q","k":"p","l":"o","m":"n"}
-
 def io():
     text=input("enter text")
     return text
@@ -41,38 +22,38 @@ def xor(text):
 
 print(xor(io()))
          
-# def write():
-#     with open("daniel.txt","w") as d:
-#         d.write(xor(io(),xorr))
+def write():
+    with open("daniel.txt","w") as d:
+        d.write(xor(io(),xorr))
 
-# def read():
-#     with open("daniel.txt","r")as r:
-#         text=r.read()
-#     return text
+def read():
+    with open("daniel.txt","r")as r:
+        text=r.read()
+    return text
 
  
  
 
-# def write():
-#     with open("daniel.txt","w") as w:
-#         w.write("daniel daidov\ndaniel levi\nbarak\ndavid")
+def write():
+    with open("daniel.txt","w") as w:
+        w.write("daniel daidov\ndaniel levi\nbarak\ndavid")
     
 
-# def read():
-#     i=0
-#     with open("daniel.txt","r")as r:
-#         for line in r:
-#             if i%2==0:
-#                 with open("daniel1.txt","w") as a:
-#                     a.write(line)
-#             if i%2!=0:
-#                 with open("daniel2.txt","w") as a:
-#                     a.write(line)
-#             i+=1
+def read():
+    i=0
+    with open("daniel.txt","r")as r:
+        for line in r:
+            if i%2==0:
+                with open("daniel1.txt","w") as a:
+                    a.write(line)
+            if i%2!=0:
+                with open("daniel2.txt","w") as a:
+                    a.write(line)
+            i+=1
 
     
-# write()
-# read()
+write()
+read()
 
 
 
